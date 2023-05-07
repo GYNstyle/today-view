f2ed2045db40c1e0584a330e49f9866f019fd92a
guoguo <guoguo@mini-m1.local>
2023-05-07 21:26:36 +0800

    Add home page

---
+++ app/today/index.tsx
@@ -0,0 +1,73 @@
+"use client";
+import Image, { StaticImageData } from "next/image";
+import { useEffect, useState, ReactNode } from "react";
+import defaultImage from "./sz.png";
+
+type UserType = {
+  id: string;
+  name: string;
+  social?: string;
+};
+type Location = {
+  url: string;
+  name: string;
+};
+type TodayView = {
+  img: string | StaticImageData;
+  desc: ReactNode;
+  user: UserType;
+  location: Location;
+};
+
+const defaultView: TodayView = {
+  img: defaultImage,
+  desc: (
+    <span>
+      This is morning view from ShenZhen. The bridge in is called{" "}
+      <a href="https://zh.wikipedia.org/wiki/%E6%B7%B1%E5%9C%B3%E7%81%A3%E5%85%AC%E8%B7%AF%E5%A4%A7%E6%A9%8B">
+        Shenzhen Bay Bridge
+      </a>
+      . It connects ShenZhen and Hong Kong. You can see the sunlight go through
+      the cloud and hit on the sea. What a becautiful view.
+    </span>
+  ),
+  user: {
+    id: "0",
+    name: "GYN",
+  },
+  location: {
+    name: "ShenZhen, China",
+    url: "https://www.google.com/search?q=%E6%B7%B1%E5%9C%B3",
+  },
+};
+
+const TodayView = () => {
+  const [todayView, setTodayView] = useState(defaultView);
+  // Get today's view
+  useEffect(() => {}, []);
+  return (
+    <div className="mt-5 mb-5 flex">
+      <Image
+        className="rounded-3xl shadow-2xl"
+        alt={"Today Picture"}
+        src={todayView.img}
+        width={450}
+      />
+      <div className="pl-10 flex flex-col w-80">
+        <div className="pb-10 flex flex-col">
+          <span className="text-3xl pb-2">The Story</span>
+          <span>{todayView.desc}</span>
+        </div>
+        <span className="pb-2">
+          Author: <a href={todayView.user.social}>{todayView.user.name}</a>
+        </span>
+        <span>
+          Location:{" "}
+          <a href={todayView.location.url}>{todayView.location.name}</a>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default TodayView;
